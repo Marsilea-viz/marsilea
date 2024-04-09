12f61ced19942553115b22b7ac6e25c24b2a6349
Mr-Milk <yzheng@cemm.at>
2024-04-09 14:08:52 +0200

    Add entrypoint for ZeroWidth and ZeroHeight

--- src/marsilea/__init__.py
+++ src/marsilea/__init__.py
@@ -4,7 +4,7 @@ __version__ = "0.3.3"
 
 import marsilea.plotter as plotter
 from ._deform import Deformation
-from .base import WhiteBoard, ClusterBoard
+from .base import WhiteBoard, ClusterBoard, ZeroWidth, ZeroHeight
 from .dataset import load_data
 from .dendrogram import Dendrogram, GroupDendrogram
 from .heatmap import Heatmap, SizedHeatmap, CatHeatmap
